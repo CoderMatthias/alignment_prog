0953cbebfc5ded955d48c80d92263145b50e50ac
Matthias <Matt.Kanke@gmail.com>
2015-07-16 16:34:12 -0400

    working aligner for proteins

--- MUSCLE_ALIGNER.sh
+++ MUSCLE_ALIGNER.sh
@@ -6,8 +6,8 @@ for specie in $(cat species_list.txt); do
   ./muscle3.8.31_i86linux64 -in $specie -out ${specie::-6}_aligned.fasta -quiet 
 done
 
-python combine_alignments.py *pairwise_aligned.fasta
+python combine_pairwise_alignments.py *pairwise_aligned.fasta
 
 rm species_list.txt
-#rm *pairwise_aligned.fasta
+rm *pairwise_aligned.fasta
 rm *pairwise.fasta
